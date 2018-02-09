da95a8335ab15f4587e4654f2fea9f393378316e
Websites-test <melissarosalesmora@gmail.com>
2018-02-09 10:11:20 -0600

    se agrega direccion de mapa

--- js/main.js
+++ js/main.js
@@ -476,5 +476,15 @@ $(document).ready(function(){
 		 $(".carousel-indicators li").remove();
 	});
 
+	$('.container.navbar a').click(function(){ 
+
+    	var $target = $($(this).data('target')); 
+
+    	if(!$target.hasClass('in'))
+
+        	$('.container.in').removeClass('in').height(0);
+
+	});
+
 });
 
